874b79467290554876d525f706637b42cf05648f
raylu15742cs <raylucs15742@gmail.com>
2023-02-01 11:48:06 -0800

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Routes, Route} from "react-router-dom";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hi</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout/>}>
+        
+      </Route>
+    </Routes>
   );
 }
 
