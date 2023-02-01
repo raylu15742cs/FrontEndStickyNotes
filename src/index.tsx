d18ba66fb58d8ec24d18e6b9f2d0ab6d745404bd
raylu15742cs <raylucs15742@gmail.com>
2023-02-01 11:42:02 -0800

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,11 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
